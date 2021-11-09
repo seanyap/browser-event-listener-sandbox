2ddbbb50d625771330f41e16a5891cd7708bca3e
Sean Yap <syap0317@gmail.com>
2021-11-09 15:53:39 -0500

    made font size bigger when switch to display key pressed

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const info = document.getElementById("key-info");
 
 window.addEventListener("keypress", (event) => {
   heading.textContent = `${event.key}`;
+  heading.style.fontSize = "5rem";
   info.textContent = `Keycode: ${event.keyCode}`;
 });
