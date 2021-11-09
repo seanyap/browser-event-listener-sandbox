a531ff2912541c4509a27d79d95dee5b3695ba5a
Sean Yap <syap0317@gmail.com>
2021-11-09 16:03:34 -0500

    added comment

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const heading = document.getElementById("key");
 const info = document.getElementById("key-info");
 
+// listens to the window for any key presses from user
 window.addEventListener("keypress", (event) => {
   heading.textContent = `${event.key}`;
   heading.style.fontSize = "5rem";
+
   info.textContent = `Keycode: ${event.keyCode}`;
 });
