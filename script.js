2662318de797cbd9474199d3b39089e934aa5fc3
Sean Yap <syap0317@gmail.com>
2021-11-09 15:44:20 -0500

    remove console logs statements

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const heading = document.getElementById("key");
 const info = document.getElementById("key-info");
-console.log(heading);
-console.log(info);
 
 window.addEventListener("keypress", (event) => {
-  console.log(event);
   heading.textContent = `${event.key}`;
   info.textContent = `Keycode: ${event.keyCode}`;
 });
